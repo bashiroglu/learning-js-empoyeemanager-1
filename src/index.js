ce7eca5ae62a99a140e427b2051c97ed61312aba
unknown <abdullabashir32@gmail.com>
2019-11-21 17:36:52 +0100

    added Post and get request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Test");
+// imports
+
+import { Request } from "./requests";
+
+// Selecting elements
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const sallaryInput = document.getElementById("sallary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+// Object creating
+const request = new Request("http://localhost:3000/employees");
+
+request
+  .get()
+  .then(employees => console.log(employees))
+  .catch(err => console.log(err));
+
+// request
+//   .post({ name: "Ismayil", department: "engineering", sallary: "7000" })
+//   .then(employee => console.log(employee))
+//   .catch(err => console.log(err));
